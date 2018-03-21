0c8ca54f11c122fdd630b337c9875045ea0c6c98
cndgus0403 <33653014+cndgus0403@users.noreply.github.com>
2018-03-21 12:54:15 +0900

    연습1

---
+++ 1.py
@@ -0,0 +1,15 @@
+print("Problem 1")
+
+total = 0
+
+while True:
+    num = int(input("Enter your number: "))
+    if num >= 20:
+        print("this number is not available value ")
+        break
+
+    else:
+            total = total + i
+        print(total)
+        total = 0
+        print("Finish!! :) ")
